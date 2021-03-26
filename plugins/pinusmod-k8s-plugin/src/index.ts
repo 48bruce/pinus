291589e8ca9d6bd5634951becd8464360504ba88
bruce48 <bruce48@foxmail.com>
2021-03-26 15:37:11 +0800

    FIX: log4js memory rising issue

---
+++ plugins/pinusmod-k8s-plugin/src/index.ts
@@ -0,0 +1,5 @@
+import { K8sPlugin } from './k8sPlugin';
+
+export function createK8sPlugin() {
+    return new K8sPlugin();
+}
---
+++ plugins/pinusmod-k8s-plugin/src/k8sPlugin.ts
@@ -0,0 +1,10 @@
+﻿import { IPlugin } from 'pinusmod';
+import { K8sComponent } from './components/k8sComponent';
+
+/**
+ * 实现 k8s 插件
+ */
+export class K8sPlugin implements IPlugin {
+    name = 'k8sPlugin';
+    components = [K8sComponent];
+}
